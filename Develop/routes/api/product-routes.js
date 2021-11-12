8363f6d2f9b082a944ec23865256ddccee27500c
Zuke27 <wstaylor27@gmail.com>
2021-11-11 20:55:37 -0500

    Updated product route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -51,7 +51,14 @@ router.post('/', (req, res) => {
  console.log(req.body)
   Product.create(req.body)
     .then((product) => {
+      const req = {
+        body: {
+          
+        }
+      }
+
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      // if(req.body.tagIds && req.body.tagIds.length) { 
       if (req.body?.tagIds?.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
